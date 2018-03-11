f1d0a1bee2eb2625afde390a5074b4444f507cee
Dillies Benoît <benoit.dillies@ynov.com>
2018-03-11 22:09:11 +0100

    ajout des speudo au chat
    utilisation des speudo en bdd pas encore efféctué (pb session/socket.io)

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -1,12 +1,19 @@
 var socket = io();
+var tempUsername = prompt("Entrez votre pseudo :");
+
+socket.on('username')
 
 $('form').submit(function () {
     data = $('#msg').val()
-    socket.emit('chat message', data);
+    socket.emit('chat message', {
+        msg: data,
+        username: tempUsername
+    });
     $('#msg').val('');
     return false;
 });
 
 socket.on('chat message', function (msg) {
-    $('#messages').append($('<li>').text(msg));
+    $('#messages').append($('<li>').text(msg.username+' : '+msg.msg));
 });
+
--- sockets/Base.js
+++ sockets/Base.js
@@ -1,5 +1,7 @@
 module.exports = (io) => {
     io.on('connection', function(socket){
+        //socket.emit('username', session.username);
+
         socket.on('chat message', function(msg){
           io.emit('chat message', msg);
         });
